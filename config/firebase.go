ba5b3f55136f1fba5c76a8caa113010623cbde62
Chandra <candrapurnama440@gmail.com>
2025-04-08 01:48:55 +0700

    `feat: Update Firebase configuration to use environment variable for credentials`

--- config/firebase.go
+++ config/firebase.go
@@ -3,6 +3,7 @@ package config
 import (
 	"context"
 	"log"
+	"os"
 
 	firebase "firebase.google.com/go"
 	"firebase.google.com/go/db"
@@ -12,23 +13,28 @@ import (
 var FirebaseClient *db.Client
 
 func InitFirebase() {
-	ctx := context.Background()
-	opt := option.WithCredentialsFile("/app/serviceAccountKey.json")
+	// Ambil credentials dari environment variable FIREBASE_CREDENTIALS
+	creds := os.Getenv("FIREBASE_CREDENTIALS")
+	if creds == "" {
+		log.Fatal("FIREBASE_CREDENTIALS environment variable not set")
+	}
 
-	conf := &firebase.Config{
+	// Inisialisasi Firebase dengan kredensial dari env
+	opt := option.WithCredentialsJSON([]byte(creds))
+	config := &firebase.Config{
 		DatabaseURL: "https://smartvehiclesentinel-2ed68-default-rtdb.asia-southeast1.firebasedatabase.app/",
 	}
 
-	app, err := firebase.NewApp(ctx, conf, opt)
+	app, err := firebase.NewApp(context.Background(), config, opt)
 	if err != nil {
-		log.Fatalf("Gagal inisialisasi Firebase: %v", err)
+		log.Fatalf("Error init Firebase App: %v", err)
 	}
 
-	client, err := app.Database(ctx)
+	client, err := app.Database(context.Background())
 	if err != nil {
-		log.Fatalf("Gagal koneksi ke Firebase DB: %v", err)
+		log.Fatalf("Error connect ke Firebase DB: %v", err)
 	}
 
+	log.Println("Firebase initialized successfully")
 	FirebaseClient = client
-	log.Println("✅ Firebase Realtime Database berhasil dikoneksi.")
 }
--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"smartVehicleSentinel/config"
 	"smartVehicleSentinel/routes"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	// Inisialisasi koneksi Firebase
+	config.InitFirebase()
+
 	r := gin.Default()
 	routes.SetupRoutes(r)
 	r.Run(":8080")
